a22c69972adf928e8d5e55d7b44a36495bfebf84
thofstrand <keerplm@gmail.com>
2023-02-14 18:20:03 -0600

    Added file upload

--- frontend-proj/src/App.vue
+++ frontend-proj/src/App.vue
@@ -1,33 +1,56 @@
 <template>
-  <div>
-    <p>hello world</p>
-    <p>no</p>
-
+  <div class="panel">
+    <label for="image-upload">Upload an image:</label>
+    <input id="image-upload" type="file" accept="image/*" v-on:change="previewImage">
+    <div v-if="imagePreview">
+      <img :src="imagePreview" alt="Preview image" class="preview-image">
+    </div>
   </div>
-  
-  
-  
 </template>
 
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const imagePreview = ref(null);
+
+    const previewImage = (event) => {
+      const fileInput = event.target;
+      if (fileInput.files && fileInput.files.length > 0) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          imagePreview.value = reader.result;
+        };
+        reader.readAsDataURL(fileInput.files[0]);
+      } else {
+        imagePreview.value = null;
+      }
+    };
+
+    return {
+      imagePreview,
+      previewImage,
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.panel {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 250px;
+  height: 100%;
+  background-color: #79acf8;
+  padding: 20px;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.preview-image {
+  max-width: 100%;
+  max-height: 200px;
+  margin-top: 20px;
 }
 </style>
